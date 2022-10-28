dec922938a36c268172a1795164d044c41f62289
soundsinteresting <441310082@qq.com>
2022-10-28 12:47:55 -0400

    fix bug in plottting

--- ppca.py
+++ ppca.py
@@ -174,7 +174,6 @@ class Experiment():
         print(args)
         from imgpro import gen_img_data
         Y = gen_img_data(args)
-        Y = add_sparse_noise([Yi for Yi in Y],0.01,500)
         print('number of images %d'%len(Y))
         args['num_client'] = len(Y)
         args['d'] = len(Y[0][0])
@@ -342,13 +341,12 @@ class Experiment():
             'nlc':1,
             'ngc':1,
             'num_dp_per_client':100,
-            'global_epochs':100,
-            'local_epochs':10,
+            'global_epochs':1000,
             'n_power':1,
-            'lr':0.001,
-            'eta':0.01,
+            'eta':0.1,
             'rho':1,
             'decay':1-0.05,
+            'logprogress':1,
         }
 
         #num_client=20
@@ -360,62 +358,63 @@ class Experiment():
         lsigma = 2
         theta1 = np.random.rand(args['num_dp_per_client'])*2*np.pi
         theta1 = theta1.reshape(len(theta1),1)
-        # Y has dimension (n_client, num_dp, d)
         Y1 = gsigma*np.cos(theta1)*gcs+lsigma*np.sin(theta1)*lcs[0]
         theta2 = np.random.rand(args['num_dp_per_client'])*2*np.pi
         theta2 = theta1.reshape(len(theta2),1)
 
         Y2 = gsigma*np.cos(theta2)*gcs+lsigma*np.sin(theta2)*lcs[1]
+        
         fig = plt.figure()
         ax = fig.add_subplot(projection='3d')
-        ax.scatter(Y1[:,0], Y1[:,1], Y1[:,2], color='pink')
+        ax.scatter(Y1[:,0], Y1[:,1], Y1[:,2], color='red')
         ax.scatter(Y2[:,0], Y2[:,1], Y2[:,2], color='blue',alpha=0.5)
-        Y = np.stack((Y1,Y2))
-        U_glb = initial_u(Y, d=args['d'], ngc=args['nlc']+args['ngc'])
+        #Y = np.stack((Y1,Y2))
+        U_glb = initial_u([Y1,Y2], d=args['d'], ngc=args['nlc']+args['ngc'])
         UNIFORM = True
         scale = 2.1
         U_glb *= scale
         if UNIFORM:
             for i in range(2):
-                if U_glb[0, 0] < 0:
-                    U_glb *= -1
+                if U_glb[2, i] < 0:
+                    U_glb[:,i] *= -1
+                
                 ax.quiver(
                         0, 0, 0,  # <-- starting point of vector
                         U_glb[0,i], U_glb[1,i], U_glb[2,i],  # <-- directions of vector
                         color='black', alpha=1., lw=2, label='global component {}'.format(i+1),
                     )
         else:
-            U, V, lv = personalized_pca_dgd(Y, args=args)
+            U, V, lv = personalized_pca_dgd([Y1,Y2], args=args)
             U = [ui*scale for ui in U]
             V = [vi*scale for vi in V]
             #print(U)
-            if U[0][0,0]<0:
+            if U[0][2,0]<0:
                 U[0] *= -1
             ax.quiver(
                     0, 0, 0,  # <-- starting point of vector
                     U[0][0,0], U[0][1,0], U[0][2,0],  # <-- directions of vector
                     color='black', alpha=1., lw=2, label='global component'
                 )
-            if V[0][0,0]<0:
+            if V[0][2,0]<0:
                 V[0] *= -1
             ax.quiver(
                 0, 0, 0,  # <-- starting point of vector
                 V[0][0, 0], V[0][1, 0], V[0][2, 0],  # <-- directions of vector
-                color='pink', alpha=1., lw=2, label='dataset 1\'s local component',
+                color='red', alpha=1., lw=2, label='node 1\'s local component',
             )
-            if V[1][0,0]<0:
+            if V[1][2,0]<0:
                 V[1] *= -1
             ax.quiver(
                 0, 0, 0,  # <-- starting point of vector
                 V[1][0, 0], V[1][1, 0], V[1][2, 0],  # <-- directions of vector
-                color='blue', alpha=1., lw=2, label='dataset 2\'s local component',
+                color='blue', alpha=1., lw=2, label='node 2\'s local component',
             )
         ax.set_xlim3d(-2.5, 2.5)
         ax.set_ylim3d(-1.5, 1.5)
         ax.set_zlim3d(-2.01, 2.01)
         ax.view_init(40, -50)
         plt.legend(prop={'size': 14})
-        plt.savefig('intro_example_ppca_{}.png'.format(not UNIFORM),bbox='tight')
+        plt.savefig('intro_example_ppca_{}.png'.format(not UNIFORM),bbox_inches='tight')
 
 
     def toy_example1(inputargs):
